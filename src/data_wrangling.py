b6f2626c80ce4337d6f49bd97b62fcd9027391a0
Luke Lage <lukelage646@gmail.com>
2026-05-22 17:19:58 -0300

    Initial data wragling & name change

---
+++ src/data_wrangling.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+from data_processsing import (
+    stores_sales_data,
+    sales_calendar_data,
+    products_sales_data,
+    sales_customers_data,
+    customers_data
+)
+
+sales_by_store = stores_sales_data.groupby('store_id')
+quantity_by_store = sales_by_store['quantity'].sum()
+revenue_by_store = sales_by_store['revenue'].sum()
+profit_by_store = sales_by_store['profit'].sum()
+
+best_day_of_week = sales_calendar_data.groupby('day_of_week')['revenue'].sum().idxmax()
+best_month = sales_calendar_data.groupby('month')['revenue'].sum().idxmax()
+best_year = sales_calendar_data.groupby('year')['revenue'].sum().idxmax()
+best_day_month = sales_calendar_data.groupby('day')['revenue'].sum().idxmax()
+
+season_counts = {
+    'Spring': sales_calendar_data['month'].isin([3, 4, 5]).sum(),
+    'Summer': sales_calendar_data['month'].isin([6, 7, 8]).sum(),
+    'Autumn': sales_calendar_data['month'].isin([9, 10, 11]).sum(),
+    'Winter': sales_calendar_data['month'].isin([12, 1, 2]).sum()
+}
+best_season = max(season_counts, key=season_counts.get)
+
+best_store = revenue_by_store.idxmax()
+best_product = products_sales_data.groupby('product_id')['revenue'].sum().idxmax()
+
+loyalty = customers_data.groupby('loyalty_member').size().rename('Customer Count')
+loyalty_percentage = loyalty / loyalty.sum() * 100
+
+loyalty_new = loyalty.to_frame()
+loyalty_new['Bool'] = loyalty_new.index.astype(bool)
